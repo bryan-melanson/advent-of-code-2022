2274ca1c903938416382cca525b5d235811b0106
Bryan Melanson <bryan@getmysa.com>
2022-11-30 12:15:59 -0330

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : Bryan Melanson (bryan@bryanmelanson.com)
+# @file        : setup
+# @created     : Wednesday Nov 30, 2022 12:13:52 NST
+#
+# @description : 
+######################################################################
+
+#!/bin/bash
+for i in {1..31}
+do
+   cp -rf template day$i
+done
+
