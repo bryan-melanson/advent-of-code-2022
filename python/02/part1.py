d781090ae99c59197ab883336e7faedd679ad7c9
Bryan Melanson <bryan.melanson@gmail.com>
2022-12-02 23:29:09 -0330

    New solution for day 2 Python

--- python/02/part1.py
+++ python/02/part1.py
@@ -1,41 +1,21 @@
-value_map = {'X': 1, 'Y': 2, 'Z': 3}
-scores = {'win': 6, 'draw': 3, 'lose': 0}
-
-def rules(b,a):
-	score = 0
-	if a == 'X':
-		if b == 'A':
-			score = value_map['X'] + scores['draw']
-		elif b == 'B':
-			score = value_map['X'] + scores['lose']
-		else:
-			score = value_map['X'] + scores['win']
-	if a == 'Y':
-		if b == 'A':
-			score = value_map['Y'] + scores['win']
-		elif b == 'B':
-			score = value_map['Y'] + scores['draw']
-		else:
-			score = value_map['Y'] + scores['lose']
-
-	if a == 'Z':
-		if b == 'A':
-			score = value_map['Z'] + scores['lose']
-		elif b == 'B':
-			score = value_map['Z'] + scores['win']
-		else:
-			score = value_map['Z'] + scores['draw']
-	return score
+rules = {'A X': 1 + 3,
+         'A Y': 1 + 6,
+         'A Z': 1 + 0,
+         'B X': 2 + 0,
+         'B Y': 2 + 3,
+         'B Z': 2 + 6,
+         'C X': 3 + 6,
+         'C Y': 3 + 0,
+         'C Z': 3 + 3
+         }
 
 
 def part1(x):
-	val = 0
-	with open(x, "r") as f:
-		data = f.read().splitlines()
-		for line in data:
-			vals = line.split(' ')
-			val = val + rules(vals[0],vals[1])
-	return val
+    val = 0
+    with open(x, "r") as f:
+        return sum([rules[x] for x in f.read().splitlines()])
+
+    return val
 
 
 test_val = 15
