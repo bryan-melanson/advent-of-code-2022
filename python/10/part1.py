72aa9b703607df2e2b43232e28abbe848ba206df
Bryan Melanson <bryan.melanson@gmail.com>
2022-12-10 02:40:10 -0330

    Day 10 Python solution

---
+++ python/10/part1.py
@@ -0,0 +1,35 @@
+import time
+
+
+def part1(x):
+    with open(x, "r") as f:
+        cycle, r = 1, 1
+        reg = dict({20: 0, 60: 0, 100: 0, 140: 0, 180: 0, 220: 0})
+        cmds = [line.split() for line in f.read().splitlines()]
+        for cmd in cmds:
+            if cmd[0] == 'noop':
+                if cycle in reg:
+                    reg[cycle] = r
+                cycle += 1
+            else:
+                if cycle in reg:
+                    reg[cycle] = r
+                val = int(cmd[1])
+                cycle += 1
+                if cycle in reg:
+                    reg[cycle] = r
+                cycle += 1
+                r += val
+        return (sum([reg[i]*i for i in reg]))
+
+
+# --- 0.059660911560058594 seconds ---
+
+
+test_val = 13140
+if part1("input") == test_val:
+    start_time = time.time()
+    print('Solution is {}'.format(part1("input")))
+    print("--- %s seconds ---" % (time.time() - start_time))
+else:
+    print("Test failed")
---
+++ python/10/part2.py
@@ -0,0 +1,38 @@
+import time
+
+
+def pixel(c, r):
+    c = c % 40
+    if not c:
+        print('\n', end='')
+    pixels = [r-1, r, r+1]
+    if c in pixels:
+        print('#', end='')
+    else:
+        print('.', end='')
+
+
+def part2(x):
+    with open(x, "r") as f:
+        cycle, r = 0, 1
+        cmds = [line.split() for line in f.read().splitlines()]
+        for cmd in cmds:
+            if cmd[0] == 'noop':
+                pixel(cycle, r)
+                cycle += 1
+            else:
+                val = int(cmd[1])
+                pixel(cycle, r)
+                cycle += 1
+                pixel(cycle, r)
+                cycle += 1
+                r += val
+        return
+
+
+# --- 0.0033750534057617188 seconds - --
+
+start_time = time.time()
+part2("input")
+print('\n')
+print("--- %s seconds ---" % (time.time() - start_time))
