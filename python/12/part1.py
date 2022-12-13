e2e04b8d9545f1156b22ee403d01983652aa4888
Bryan Melanson <bryan.melanson@gmail.com>
2022-12-13 02:47:44 -0330

    Clean up Day 12 part 1

--- python/12/part1.py
+++ python/12/part1.py
@@ -9,26 +9,25 @@ def part1(col):
             for col in range(len(data[0])):
                 if data[row][col] == "S":
                     start = (row, col)
-                    data[row][col] = "z"
                 if data[row][col] == "E":
                     end = (row, col)
-                    data[row][col] = "z"
+                    data[row][col] = 'z'
 
         queue = deque([(start[0], start[1], 0)])
         visited = set()
 
-        while queue:
+        while queue > 0:
             row, col, val = queue.popleft()
             if (row, col) == end:
                 return val
             if (row, col) in visited:
                 continue
             visited.add((row, col))
-            for c, r in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+            for r, c in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
                 if (len(data) > row+r >= 0 and len(data[0]) > col+c >= 0):
-                    if ord(data[row+r][col+c]) <= ord(data[row][col])+1:
+                    if ((row, col) == start) or (ord(data[row+r][col+c]) <= ord(data[row][col])+1):
                         queue.append((row+r, col+c, val+1))
-        return 0
+        return val
 
 # --- 0.055726051330566406 seconds ---
 
