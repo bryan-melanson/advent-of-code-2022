2fc2822e3ba6b7b38dfdb8a3e3e89f31bb1c94bc
Bryan Melanson <bryan.melanson@gmail.com>
2022-12-14 12:54:37 -0330

    Update templates

--- template/python/part1.py
+++ template/python/part1.py
@@ -1,11 +1,11 @@
 def part1(x):
-	with open(x, "r") as f:
-		data = f.read().splitlines()
-	val = 0
-	return val
+    with open(x, "r") as f:
+    data = f.read().splitlines()
+    val = 0
+    return val
 
 test_val = 0
 if part1("test") == test_val:
-	print('Solution is {}'.format(part1("input")))
+    print('Solution is {}'.format(part1("input")))
 else:
-	print("Test failed")
+    print("Test failed")
--- template/python/part2.py
+++ template/python/part2.py
@@ -1,11 +1,12 @@
 def part2(x):
-	with open(x, "r") as f:
-		data = f.read().splitlines()
-	val = 0
-	return val
+    with open(x, "r") as f:
+        data = f.read().splitlines()
+    val = 0
+    return val
+
 
 test_val = 0
 if part2("test") == test_val:
-	print('Solution is {}'.format(part2("input")))
+    print('Solution is {}'.format(part2("input")))
 else:
-	print("Test failed")
+    print("Test failed")
