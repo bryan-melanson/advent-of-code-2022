1c8d7780c25be8f97e3e6415d485ab0788fa2648
Bryan Melanson <bryan.melanson@gmail.com>
2022-12-14 14:44:54 -0330

    Update templates

--- template/python/part1.py
+++ template/python/part1.py
@@ -1,11 +1,15 @@
+from time import time
+
+
 def part1(x):
     with open(x, "r") as f:
-    data = f.read().splitlines()
-    val = 0
-    return val
+        return f.read().splitlines()
+
 
 test_val = 0
 if part1("test") == test_val:
+    start_time = time()
     print('Solution is {}'.format(part1("input")))
+    print("--- %s seconds ---" % (time.time() - start_time))
 else:
     print("Test failed")
--- template/python/part2.py
+++ template/python/part2.py
@@ -1,12 +1,14 @@
+from time import time
+
 def part2(x):
     with open(x, "r") as f:
-        data = f.read().splitlines()
-    val = 0
-    return val
+        return f.read().splitlines()
 
 
 test_val = 0
 if part2("test") == test_val:
+    start_time = time()
     print('Solution is {}'.format(part2("input")))
+    print("--- %s seconds ---" % (time.time() - start_time))
 else:
     print("Test failed")
