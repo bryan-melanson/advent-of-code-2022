e7e69899b9acdea89efe80ca19f01d62c7e9f09f
Bryan Melanson <bryan.melanson@gmail.com>
2022-12-15 00:24:03 -0330

    Day 13 Python Part 2 solution

--- python/13/part2.py
+++ python/13/part2.py
@@ -1,46 +1,53 @@
 import time
-from collections import deque
+import ast
 
 
-def checkPath(start, end, nodes):
-    val = 0
-    queue = deque([(start[0], start[1], val)])
-    visited = set()
-    while queue:
-        row, col, val = queue.popleft()
-        if (row, col) == end:
-            return val
-        if (row, col) in visited:
-            continue
-        visited.add((row, col))
-        for r, c in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
-            if (len(nodes) > row+r >= 0 and len(nodes[0]) > col+c >= 0):
-                if (ord(nodes[row+r][col+c]) <= ord(nodes[row][col])+1):
-                    queue.append((row+r, col+c, val+1))
-    return 999
+def check(a, b):
+    VALID = 1
+    INVALID = -1
+    NA = 0
+    for idx in range(max(len(a), len(b))):
+        if idx >= len(a):
+            return VALID
+        if idx >= len(b):
+            return NA
+        l, r = a[idx], b[idx]
+        valid = INVALID
+        if type(l) == int and type(r) == int:
+            if l < r:
+                valid = VALID
+            elif l > r:
+                valid = NA
+            else:
+                valid = INVALID
+                continue
+        elif type(l) == list and type(r) == list:
+            valid = check(l, r)
+        elif type(l) == list and type(r) == int:
+            valid = check(l, [r])
+        elif type(l) == int and type(r) == list:
+            valid = check([l], r)
+        if valid != INVALID:
+            return valid
+    return INVALID
 
 
-def part2(col):
-    with open(col, "r") as f:
-        data = [list(line) for line in f.read().splitlines()]
-        start, end = (0,0)
-        starts, vals = [],[]
-        for row in range(len(data)):
-            for col in range(len(data[0])):
-                if data[row][col] == 'S':
-                    data[row][col] = 'a'
-                    starts.append((row, col))
-                if data[row][col] == 'a':
-                    starts.append((row, col))
-                if data[row][col] == "E":
-                    end = (row, col)
-                    data[row][col] = 'z'
-        for start in starts:
-            vals.append(checkPath(start, end, data))
-        return(min(vals))
-# --- 2.41329789162 seconds ---
+def part2(x):
+    with open(x, "r") as f:
+        pairs = [ast.literal_eval(pair)
+                 for pair in f.read().splitlines() if pair != '']
+        x, y = 1, 2
+        for i in range(len(pairs)):
+            if (check(pairs[i], [[2]])):
+                x += 1
+            if (check(pairs[i], [[6]])):
+                y += 1
+        return x*y
 
-test_val = 29
+# --- 0.06795477867126465 seconds ---
+
+
+test_val = 140
 if part2("test") == test_val:
     start_time = time.time()
     print('Solution is {}'.format(part2("input")))
