1286a8487da55ad9acf84be4b13b55ea996dc5a8
Bryan Melanson <bryan.melanson@gmail.com>
2023-05-18 23:40:42 -0230

    Add Day 1a Rust solution

--- rust/01/src/part2.rs
+++ rust/01/src/part2.rs
@@ -1,6 +1,9 @@
-fn solution(path: String) -> u32 {
-    let _data = std::fs::read_to_string(path);
-    0
+fn solution(data: String) -> u32 {
+    let val = data.split("\n\n")
+        .map(|e| e.lines().map(|c| c.parse::<u32>().unwrap()).sum::<u32>())
+        .max()
+        .unwrap();
+    val
 }
 
 pub fn print_solution(test_val: u32) {
