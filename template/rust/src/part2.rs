81c3e08192c464b71ca071651af3ec069d689e8e
Bryan Melanson <bryan.melanson@gmail.com>
2023-05-22 23:51:08 -0230

    Time Rust executions

--- template/rust/src/part2.rs
+++ template/rust/src/part2.rs
@@ -1,8 +1,12 @@
+use std::time::Instant;
+
 fn solution(_data: String) -> u32 {
     0
 }
 
 pub fn print_solution(test_val: u32) {
+    let start = Instant::now();
+
     let input = std::fs::read_to_string("./input").unwrap();
     let test = std::fs::read_to_string("./test").unwrap();
 
@@ -10,5 +14,7 @@ pub fn print_solution(test_val: u32) {
         println!("Test failed");
     } else {
         println!("{}", solution(input));
+        let duration = start.elapsed();
+        println!("Elapsed time: {:.2?}", duration);
     }
 }
